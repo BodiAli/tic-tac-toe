678494b003bd875d2ead99030d0353f78556bc52
Bodi_Ali <bodiali59@gmail.com>
2024-01-20 05:29:47 +0200

    Add check win function

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
+function createPlayer (name, marker){
+      
+  return {name, marker}
+}
+
+
 
   const Gameboard = {
     gameBoard: [],
     init: function(){
       this.cacheDom()
       this.bindEvents()
-    },
-    createPlayer: function(name, marker){
-      
-      return {name, marker}
     },
     cacheDom: function(){
       this.cells = document.querySelectorAll(".cells");
@@ -19,83 +21,93 @@
       }, this)
     },
     addGameFlow: function(){
-      // this.cells.forEach(function(element){
-      //   if (element.textContent){
-      //     this.gameBoard.push(element.textContent)
-      //   }
-      // },this);
-      this.gameBoard[0] = this.cells[0].textContent
-      this.gameBoard[1] = this.cells[1].textContent
-      this.gameBoard[2] = this.cells[2].textContent
-      this.gameBoard[3] = this.cells[3].textContent
-      this.gameBoard[4] = this.cells[4].textContent
-      this.gameBoard[5] = this.cells[5].textContent
-      this.gameBoard[6] = this.cells[6].textContent
-      this.gameBoard[7] = this.cells[7].textContent
-      this.gameBoard[8] = this.cells[8].textContent
     },
     render: function(ev){
-      ev.target.textContent = "X"
-      for (let i = 0; i < 10; i++){
-        let choice =  Math.floor(Math.random() * 9);
-        if (this.cells[choice].textContent === "X" || this.cells[choice].textContent === "O"){
-          continue;
-        }
-        else {
-          this.cells[choice].textContent = "O"
-          break;
-        }
+      if (ev.target.textContent === ""){
+      ev.target.textContent = this.player.marker;
+      this.generateComputerChoice(ev)
+    }
       
-      }
     },
     addMarker: function(ev){
-      this.createPlayer("bodi", "X")
-      switch (ev.target.classList[0]){
-        case 0:
-          this.addGameFlow();
-          break;
-        case 1:
-          this.addGameFlow();
-          break;
-        case 2: 
-          this.addGameFlow();
-          break;
-        case 3: 
-          this.addGameFlow();
-          break;
-        case 4: 
-          this.addGameFlow();
-          break;
-        case 5: 
-          this.addGameFlow();
-          break;
-        case 6: 
-          this.addGameFlow();
-          break;
-        case 7: 
-          this.addGameFlow();
-          break;
-        case 8: 
-          this.addGameFlow();
-          break;
-      }
-      this.render(ev)
-      this.addGameFlow()
+    this.render(ev)
+    this.gameBoard[0] = this.cells[0].textContent
+      
+    this.gameBoard[1] = this.cells[1].textContent
+
+    this.gameBoard[2] = this.cells[2].textContent
+
+    this.gameBoard[3] = this.cells[3].textContent
+
+    this.gameBoard[4] = this.cells[4].textContent
+
+    this.gameBoard[5] = this.cells[5].textContent
+
+    this.gameBoard[6] = this.cells[6].textContent
+
+    this.gameBoard[7] = this.cells[7].textContent
+
+    this.gameBoard[8] = this.cells[8].textContent
+    this.checkWin(ev)
+
     },
-    computerPlayer: function(){
-      this.createPlayer("Computer", "O")
+   player: createPlayer("Bodi", "X"),
+   computerPlayer: createPlayer("Computer", "O"),
+
+   generateComputerChoice: function(ev){
+     for (let i = 0; i < 10; i++){
+      //  let choice =  Math.floor(Math.random() * 9);
+      //  if (this.cells[choice].textContent === "X" || this.cells[choice].textContent === "O"){
+      //    continue;
+      //   } else { 
+      //    this.cells[choice].textContent = this.computerPlayer.marker
+      //    break;
+      //  }
+      if (this.gameBoard[i] === "X" || this.gameBoard[i] === "O"){
+        continue;
+      } else {
+        this.gameBoard[2] = "O"
+        break;
+      }
+        
+      }
+    },
+   checkWin: function(ev){
+    if(
+      this.gameBoard[0] === "X" && this.gameBoard[1] === "X" && this.gameBoard[2] === "X" ||
+      this.gameBoard[0] === "X" && this.gameBoard[3] === "X" && this.gameBoard[6] === "X" ||
+      this.gameBoard[0] === "X" && this.gameBoard[4] === "X" && this.gameBoard[8] === "X" ||
+      this.gameBoard[1] === "X" && this.gameBoard[4] === "X" && this.gameBoard[7] === "X" ||
+      this.gameBoard[2] === "X" && this.gameBoard[4] === "X" && this.gameBoard[6] === "X" ||
+      this.gameBoard[2] === "X" && this.gameBoard[5] === "X" && this.gameBoard[8] === "X" ||
+      this.gameBoard[6] === "X" && this.gameBoard[7] === "X" && this.gameBoard[8] === "X" ||
+      this.gameBoard[3] === "X" && this.gameBoard[4] === "X" && this.gameBoard[5] === "X"
+    ){
+      console.log("You Won")
+      this.cells.forEach(this.stopBoardClick)
+    } else if (
+      this.gameBoard[0] === "O" && this.gameBoard[1] === "O" && this.gameBoard[2] === "O" ||
+      this.gameBoard[0] === "O" && this.gameBoard[3] === "O" && this.gameBoard[6] === "O" ||
+      this.gameBoard[0] === "O" && this.gameBoard[4] === "O" && this.gameBoard[8] === "O" ||
+      this.gameBoard[1] === "O" && this.gameBoard[4] === "O" && this.gameBoard[7] === "O" ||
+      this.gameBoard[2] === "O" && this.gameBoard[4] === "O" && this.gameBoard[6] === "O" ||
+      this.gameBoard[2] === "O" && this.gameBoard[5] === "O" && this.gameBoard[8] === "O" ||
+      this.gameBoard[6] === "O" && this.gameBoard[7] === "O" && this.gameBoard[8] === "O" ||
+      this.gameBoard[3] === "O" && this.gameBoard[4] === "O" && this.gameBoard[5] === "O"
+
+    ) {
+      console.log("Computer Won")
+      this.cells.forEach(this.stopBoardClick)
     }
+   },
+   stopBoardClick: function(element){
+    element.classList.add("canceled")
+   }
   }
 Gameboard.init()
 
 
 console.log(Gameboard)
 
-for (let i = 0; i < 20; i++){
-  if (i === 5) {continue};
-  if (i === 10) {continue};
-  if (i === 15) {continue} ;
-  if (i === 18) {break}
-  console.log(i)
-  
-}
+const arr = ["X","O", "X", "O", "X", "O"];
+
